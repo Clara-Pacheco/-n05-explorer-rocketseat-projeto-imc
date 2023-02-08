f9d80956cf25a04f84dbae8fbf385b1996e4b77a
Clara <clarapachecodev@gmail.com>
2023-02-08 18:27:41 -0300

    Validating user's input

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ import { Modal } from './modal.js'
 const form = document.querySelector("form")
 const inputWeight = document.querySelector("#weight")
 const inputHeight = document.querySelector("#height")
+const alertError = document.querySelector('.alert-error')
 
 
 function IMC(weight, height) {
@@ -15,10 +16,21 @@ form.addEventListener('submit', (e) => {
   const weight = inputWeight.value
   const height = inputHeight.value
 
-  const imcResult = IMC(weight,height)
+  if(notANumber(weight) || notANumber(height) || notANumber("")){
+    alertError.classList.add('open')
+    return
+  }else{
+    alertError.classList.remove('open')
+    const imcResult = IMC(weight,height)
 
-  Modal.open()
-  Modal.message.innerText = `O seu IMC é de ${imcResult}`
+    Modal.open()
+    Modal.message.innerText = `O seu IMC é de ${imcResult}`
+  }
+
+  
 
 })
 
+function notANumber(value){
+  return isNaN(value)
+}
